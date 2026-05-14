aa993dfa0395b82162eacf0616b4ac6e8b495352
binhdayne <duongthanhbinh1411@gmail.com>
2026-05-14 15:14:28 +0000

    Fix Magento runtime verification issues

--- src/app/code/FashionStore/ProductSize/Setup/Patch/Data/ApplySizeOptionsToExistingProducts.php
+++ src/app/code/FashionStore/ProductSize/Setup/Patch/Data/ApplySizeOptionsToExistingProducts.php
@@ -6,6 +6,9 @@ namespace FashionStore\ProductSize\Setup\Patch\Data;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
@@ -20,30 +23,48 @@ class ApplySizeOptionsToExistingProducts implements DataPatchInterface
     /** @var ProductRepositoryInterface */
     private $productRepository;
 
+    /** @var State */
+    private $appState;
+
     public function __construct(
         ModuleDataSetupInterface $moduleDataSetup,
         ProductCollectionFactory $productCollectionFactory,
-        ProductRepositoryInterface $productRepository
+        ProductRepositoryInterface $productRepository,
+        State $appState
     ) {
         $this->moduleDataSetup = $moduleDataSetup;
         $this->productCollectionFactory = $productCollectionFactory;
         $this->productRepository = $productRepository;
+        $this->appState = $appState;
     }
 
     public function apply(): void
     {
         $this->moduleDataSetup->getConnection()->startSetup();
 
-        $productCollection = $this->productCollectionFactory->create();
-        $productCollection->addAttributeToSelect(['sku']);
-        $productCollection->addFieldToFilter('type_id', ['eq' => 'simple']);
+        try {
+            $this->ensureAreaCode();
 
-        foreach ($productCollection as $product) {
-            $fullProduct = $this->productRepository->getById((int) $product->getId(), false, null, true);
-            $this->productRepository->save($fullProduct);
+            $productCollection = $this->productCollectionFactory->create();
+            $productCollection->addAttributeToSelect(['sku']);
+            $productCollection->addFieldToFilter('type_id', ['eq' => 'simple']);
+
+            foreach ($productCollection as $product) {
+                $fullProduct = $this->productRepository->getById((int) $product->getId(), false, null, true);
+                $this->productRepository->save($fullProduct);
+            }
+        } finally {
+            $this->moduleDataSetup->getConnection()->endSetup();
         }
+    }
 
-        $this->moduleDataSetup->getConnection()->endSetup();
+    private function ensureAreaCode(): void
+    {
+        try {
+            $this->appState->getAreaCode();
+        } catch (LocalizedException $exception) {
+            $this->appState->setAreaCode(Area::AREA_ADMINHTML);
+        }
     }
 
     public static function getDependencies(): array
